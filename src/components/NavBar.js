daf996a32e23f4bf53175f28e5f2b50f241b8ce0
Shan Siddiqui <49912126+shansiddiqui94@users.noreply.github.com>
2024-02-22 01:49:08 -0500

    lab completed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,15 @@ import React from "react";
 
 function NavBar() {
   return (
-    <nav>
-      <a href="#home">I'm a link!</a>
+    <div>
+     <nav>
+     <a href="#home">Home</a>
+      <a href="#about">About</a>
     </nav>
+   
+    </div>
+
   );
 }
+
+export default NavBar
